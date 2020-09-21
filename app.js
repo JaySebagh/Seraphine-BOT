c50fae6eaf832290e7af227a356912f769c84264
JaySebagh <jaysebagh@gmail.com>
2020-09-21 14:07:00 -0700

    basic setup

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 require('dotenv').config()
-console.log(process.env.TOKEN)
+const token = process.env.TOKEN
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('message', msg => {
+    if(msg.content === 'wont look') msg.reply(':ratirlWontLook:')
+})
+
+client.login(token)
